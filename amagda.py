a16659bf3eb63d58b8f16aa3f41a3c8b1e1681e1
EmeraIdQueen <141786591+EmeraIdQueen@users.noreply.github.com>
2024-10-17 09:24:55 -0300

    Add files via upload

--- amagda.py
+++ amagda.py
@@ -3,13 +3,16 @@ from random import choice
 acoes_permitidas = ['pedra', 'papel', 'tesoura']
 ponto_AI_1 = 0
 ponto_Usuario = 0
-n = 0
+n = 0 
+#contador que controla o número de rodadas jogadas
 ultimo_movimento_AI = ''
+#evita que a IA repita o último movimento
 
 amarelo = '\033[33m'
 original = '\033[0;0m'
 negrito = '\033[1m'
 
+#funções que executam o sistema de pontuação e mostram na tela os resultados
 def ponto_ai_1():
     global ponto_AI_1
     print(amarelo + 'Ponto da AI 1\n'+ original)
@@ -20,16 +23,26 @@ def ponto_usuario():
     print(amarelo + 'Ponto do Usuario\n'+ original)
     ponto_Usuario += 1
 
+
+
 while n < 3:
+    #3 rodadas melhor de 3
     movimento_AI_1 = str(choice(acoes_permitidas))
     while movimento_AI_1 == ultimo_movimento_AI:
         movimento_AI_1 = str(choice(acoes_permitidas))
+        #isso aqui serve pra se o movimento que ela escolher for igual ela escolhe dnv
+
+    ultimo_movimento_AI = movimento_AI_1
     movimento_Usuario = input('Escolha qual opcao (pedra, papel, tesoura): ')
     movimento_Usuario.lower()
+    #transforma em minusculo o comando do usuário
+
     while movimento_Usuario not in acoes_permitidas:
         movimento_Usuario = input('Escolha qual opcao (pedra, papel, tesoura): ')
         movimento_Usuario.lower()
-        movimento_AI_1 = str(choice(acoes_permitidas))
+        #isso serve pra caso o usuário não escrever um movimentoi válido
+
+       
     print(f'AI 1 escolheu {movimento_AI_1}\nUsuario escolheu {movimento_Usuario}')
     if movimento_AI_1 == movimento_Usuario:
         print(amarelo + 'Empatou\n' + original)
