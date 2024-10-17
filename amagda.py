77f69c6e2eeb9a5e6db77fcf0aa3ecfab2d6b824
EmeraIdQueen <141786591+EmeraIdQueen@users.noreply.github.com>
2024-10-17 10:11:34 -0300

    Add files via upload

--- amagda.py
+++ amagda.py
@@ -8,6 +8,8 @@ n = 0
 ultimo_movimento_AI = ''
 #evita que a IA repita o último movimento
 
+
+#estilo das fontes
 amarelo = '\033[33m'
 original = '\033[0;0m'
 negrito = '\033[1m'
@@ -34,19 +36,20 @@ while n < 3:
 
     ultimo_movimento_AI = movimento_AI_1
     movimento_Usuario = input('Escolha qual opcao (pedra, papel, tesoura): ')
-    movimento_Usuario.lower()
+    movimento_Usuario = movimento_Usuario.lower()
     #transforma em minusculo o comando do usuário
 
     while movimento_Usuario not in acoes_permitidas:
         movimento_Usuario = input('Escolha qual opcao (pedra, papel, tesoura): ')
-        movimento_Usuario.lower()
+        movimento_Usuario = movimento_Usuario.lower()
         #isso serve pra caso o usuário não escrever um movimentoi válido
 
        
-    print(f'AI 1 escolheu {movimento_AI_1}\nUsuario escolheu {movimento_Usuario}')
+    print(f'AI escolheu {movimento_AI_1}\nUsuario escolheu {movimento_Usuario}')
     if movimento_AI_1 == movimento_Usuario:
         print(amarelo + 'Empatou\n' + original)
 
+    #resultado programado dos jogos para a AI
     elif movimento_AI_1 == 'pedra' and movimento_Usuario == 'tesoura':
         ponto_ai_1()
     elif movimento_AI_1 == 'tesoura' and movimento_Usuario == 'papel':
@@ -54,13 +57,17 @@ while n < 3:
     elif movimento_AI_1 == 'papel' and movimento_Usuario == 'pedra':
         ponto_ai_1()
     
+    #resultado programado dos jogos para o usuário
     elif movimento_Usuario == 'pedra' and movimento_AI_1 == 'tesoura':
         ponto_usuario()
     elif movimento_Usuario == 'tesoura' and movimento_AI_1 == 'papel':
         ponto_usuario()
     elif movimento_Usuario == 'papel' and movimento_AI_1 == 'pedra':
         ponto_usuario()
-    n += 1
 
+    n += 1
+    #adiciona mais 1 ao contador para chegar a 3 e encerrar a partida
+
+#Isso serve pra mostrar o resultado final do jogo
 print(negrito + amarelo + 'Pontuacao final' + original)
-print(f'AI 1 fez {ponto_AI_1}\nUsuario fez {ponto_Usuario}')
+print(f'AI fez {ponto_AI_1}\nUsuario fez {ponto_Usuario}')
